04a27feaaa63be62ebf585591fdaf881e504bce8
jmsnuwan-dotcom <jmsnuwan@gmail.com>
2026-09-14 00:37:19 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="lumora-1m-v15-vercel";
+const C="lumora-1m-v14-vercel";
 const A=["./","./index.html","./styles.css","./js/engine.js","./js/app.js","./manifest.webmanifest","./lumora-logo.png","./icons/favicon.png","./icons/lumora-192.png","./icons/lumora-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("lumora-")&&k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
